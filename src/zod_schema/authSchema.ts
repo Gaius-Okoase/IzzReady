36e4fee368ca196dfa320fe2d7bcbbdd87f9001b
Gaius Okoase Oreoluwa <gaiusonosetale.com>
2026-05-18 18:22:33 +0100

    feat(schema): add Zod schemas for user registration and login validation

---
+++ src/zod_schema/authSchema.ts
@@ -0,0 +1,15 @@
+import zod from 'zod';
+
+export const RegisterSchema = zod.object({
+    name: zod.string({
+        error: (iss) => iss.input === undefined ? "Please provide your name" : "Name must be a string",
+    }).trim().min(4, 'Name must be a minimum of 4 characters'),
+    phoneNumber: zod.string('Phone number must be a string').trim().regex(/^(\+234|0)[789][01]\d{8}$/, 'Please enter a valid phone number'),
+    password: zod.string('Password must be a string').trim().min(6, 'Password must be at least 6 characters'),
+    role: zod.literal(['customer', 'owner'], 'Please sign up with a role')
+});
+
+export const LoginSchema = zod.object({
+    phoneNumber: zod.string('Phone number must be a string').trim().regex(/^(\+234|0)[789][01]\d{8}$/, 'Please enter a valid phone number'),
+    password: zod.string('Password must be a string').trim().min(6, 'Password must be at least 6 characters')
+});
